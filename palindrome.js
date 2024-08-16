e94ace883cc04847c9913a9b6bd879f3b565e6b3
Nawab_Saab <furkanchisti786@gmail.com>
2024-08-17 00:27:50 +0530

    checking vowels in string

--- palindrome.js
+++ palindrome.js
@@ -5,3 +5,26 @@ function isPalindrome(str) {
 
 console.log(isPalindrome("racecar")); //true
 console.log(isPalindrome("hello")); //false
+
+//! To check how many vowels are there
+
+function countVowels(str) {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(
+  "For vowels count====>",
+  countVowels("batana kitne vowels hain isme") //11
+);
