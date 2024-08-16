76d175872605fb6ca5e520e53e86f9f99bb01523
Nawab_Saab <furkanchisti786@gmail.com>
2024-08-17 00:53:41 +0530

    add table

--- postPrefix.js
+++ postPrefix.js
@@ -4,7 +4,6 @@ let inc_Original_ValBy_Postfix = original_Value++;
 console.log(
   `original_Value:${original_Value}, inc_Original_ValBy_Postfix:${inc_Original_ValBy_Postfix}`
 );
-//console.table([x]);
 
 //If we use postfix, with operator after operand (for example, x++),
 //the increment operator increments and returns the value before incrementing.
@@ -13,6 +12,6 @@ let a = 3;
 console.log(`a:${a}`);
 let b = ++a;
 console.log(`a:${a}, a:${b}`);
-//console.table([x]);
+//console.table([a,b]);
 //If we use prefix, with operator before operand (for example, ++x),
 //he increment operator increments and returns the value after incrementing.
