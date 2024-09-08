18c834b2aefae18e1822360188bc68a67fa9bb4f
Nawab_Saab <furkanchisti786@gmail.com>
2024-09-08 22:10:21 +0530

    func and obj

---
+++ Functions/02_basicfuncs.js
@@ -0,0 +1,17 @@
+let gfDetails = {
+  name: {
+    firstCrushName: "Sai Pallavi",
+    seconsCrushName: "Rashmika Mandana",
+  },
+  age: {
+    pallaviAge: 24,
+    rashmikaAge: 25,
+  },
+  myMethod() {
+    console.log("hello");
+  },
+};
+console.log(gfDetails.name.firstCrushName);
+
+//node package executer
+//npx react-native init myproject
