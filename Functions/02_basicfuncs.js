6e7ee930450eb43722b6671cb6ec997f222f96e4
Nawab_Saab <furkanchisti786@gmail.com>
2024-09-09 16:57:10 +0530

    funct modifications

--- Functions/02_basicfuncs.js
+++ Functions/02_basicfuncs.js
@@ -1,17 +1,37 @@
 let gfDetails = {
   name: {
     firstCrushName: "Sai Pallavi",
-    seconsCrushName: "Rashmika Mandana",
+    seconsCrushName: "Emilia Clarke | Khalessi",
+    thirdCrushName: "Rashmika Mandana",
+    fourthCrushName: "Selena Gomez",
   },
   age: {
-    pallaviAge: 24,
+    pallaviAge: 23,
+    KhalessiAge: 24,
     rashmikaAge: 25,
+    salinAge: 26,
   },
   myMethod() {
-    console.log("hello");
+    console.log("All are accepted");
   },
 };
 console.log(gfDetails.name.firstCrushName);
+console.log(gfDetails.name);
+gfDetails.myMethod();
+
+// Convert details to a 2D array format (rows and columns)
+let crushDetailsArray = [
+  ["Crush Name", "Age"],
+  [gfDetails.name.firstCrushName, gfDetails.age.pallaviAge],
+  [gfDetails.name.seconsCrushName, gfDetails.age.KhalessiAge],
+  [gfDetails.name.thirdCrushName, gfDetails.age.rashmikaAge],
+  [gfDetails.name.fourthCrushName, gfDetails.age.salinAge],
+];
+
+// Print the array in rows and columns
+for (let i = 0; i < crushDetailsArray.length; i++) {
+  console.log(crushDetailsArray[i].join(" | "));
+}
 
 //node package executer
 //npx react-native init myproject
