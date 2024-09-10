4c399b9833ef9cf990e49992f258af2df6bb3b22
Nawab_Saab <furkanchisti786@gmail.com>
2024-09-10 22:36:59 +0530

    function op

--- Functions/02_basicfuncs.js
+++ Functions/02_basicfuncs.js
@@ -18,20 +18,3 @@ let gfDetails = {
 console.log(gfDetails.name.firstCrushName);
 console.log(gfDetails.name);
 gfDetails.myMethod();
-
-// Convert details to a 2D array format (rows and columns)
-let crushDetailsArray = [
-  ["Crush Name", "Age"],
-  [gfDetails.name.firstCrushName, gfDetails.age.pallaviAge],
-  [gfDetails.name.seconsCrushName, gfDetails.age.KhalessiAge],
-  [gfDetails.name.thirdCrushName, gfDetails.age.rashmikaAge],
-  [gfDetails.name.fourthCrushName, gfDetails.age.salinAge],
-];
-
-// Print the array in rows and columns
-for (let i = 0; i < crushDetailsArray.length; i++) {
-  console.log(crushDetailsArray[i].join(" | "));
-}
-
-//node package executer
-//npx react-native init myproject
