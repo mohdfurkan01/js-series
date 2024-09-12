0c3d6768826253861d4bc0b8a83e7b02fe1a9a4b
Nawab_Saab <furkanchisti786@gmail.com>
2024-09-12 16:14:19 +0530

    clarification for call stack

--- Functions/02_basicfuncs.js
+++ Functions/02_basicfuncs.js
@@ -28,3 +28,14 @@ gfDetails.myMethod();
 (() => {
   console.log("arrow finction IIFE");
 })();
+
+let val1 = 10;
+let val2 = 5;
+function addNum(num1, num2) {
+  let total = num1 + num2;
+  return total;
+}
+let result1 = addNum(val1, val2);
+let result2 = addNum(10, 2);
+console.log(result1); //15
+console.log(result2); //12
