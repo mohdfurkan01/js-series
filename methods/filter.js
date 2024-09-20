954388a3c263699ee15ead49f6837587611e9a05
Nawab_Saab <furkanchisti786@gmail.com>
2024-09-21 02:04:11 +0530

    made changes

--- methods/filter.js
+++ methods/filter.js
@@ -2,3 +2,9 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const newNums = myNums.filter((num) => num > 4);
 console.log(newNums);
+
+let forMap = myNums.map((item) => {
+  console.log(`for item val: ${item}`);
+  return item + 2;
+});
+console.log(forMap);
